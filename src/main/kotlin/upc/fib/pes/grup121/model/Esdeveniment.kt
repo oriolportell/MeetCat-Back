3b7a957bed3ac9e6fade3b52c3f2cb1065fc21d2
Oriol <oriol_fort@hotmail.com>
2022-10-07 19:08:09 +0200

    Creades clases esdeveniments, pendent connexio BD

--- src/main/kotlin/upc/fib/pes/grup121/model/Esdeveniment.kt
+++ src/main/kotlin/upc/fib/pes/grup121/model/Esdeveniment.kt
@@ -1,3 +1,13 @@
 package upc.fib.pes.grup121.model
 
-data class Esdeveniment()
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Table(name = "esdeveniments")
+data class Esdeveniment(
+    @Id  @GeneratedValue(strategy = GenerationType.AUTO) var id: Long,
+    val titol: String,
+    val descripcio: String
+)
--- src/main/kotlin/upc/fib/pes/grup121/service/EsdevenimentService.kt
+++ src/main/kotlin/upc/fib/pes/grup121/service/EsdevenimentService.kt
@@ -1,4 +1,29 @@
 package upc.fib.pes.grup121.service
 
-class EsdevenimentService {
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import upc.fib.pes.grup121.model.Esdeveniment
+import upc.fib.pes.grup121.repository.EsdevenimentRepository
+
+@Service
+class EsdevenimentService(val repository: EsdevenimentRepository) {
+
+    fun getAll(): List<Esdeveniment> = repository.findAll()
+
+    fun getById(id: Long): Esdeveniment = repository.findById(id).get()
+
+    fun create(esdeveniment: Esdeveniment): Esdeveniment = repository.save(esdeveniment)
+
+    fun remove(id: Long) {
+        if (repository.existsById(id)) repository.deleteById(id)
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    fun update(id: Long, esdeveniment: Esdeveniment): Esdeveniment {
+        return if (repository.existsById(id)) {
+            esdeveniment.id = id
+            repository.save(esdeveniment)
+        } else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
 }
